0e9520267a3ca658bed0c263d1c068125f206a87
aryanballani <aryanballani@gmail.com>
2024-08-24 21:46:34 -0700

    login page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app/pages/login.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,74 +10,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return  const MaterialApp(
+      title: 'Bill Splitter',
+      debugShowCheckedModeBanner: false,
+      home: LoginPage()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: GridView.count(
-          // Create a grid with 2 columns. If you change the crossAxisCount to 3, the grid will have 3 columns, etc.
-          crossAxisCount: 2,
-          mainAxisSpacing: 10.0,
-          crossAxisSpacing: 10.0,
-          padding: const EdgeInsets.all(20.0),
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Button 1'),
-            ),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Button 2'),
-            ),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Button 3'),
-            ),
-            ElevatedButton(
-              onPressed: _incrementCounter,
-              child: const Text('Button 4'),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
---
+++ lib/pages/login.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _showPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _AppBar(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              makeTextField(_usernameController, "Username", false),
+              const SizedBox(height: 16.0),
+              makeTextField(_passwordController, "Password", !_showPassword),
+              const SizedBox(height: 16.0),
+              makeShowPassword(),
+              const SizedBox(height: 24.0),
+              makeLoginButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  
+  TextField makeTextField(TextEditingController _controller, String _labelText, bool _obscureText) {
+    return TextField(
+      controller: _controller,
+      obscureText: _obscureText,
+      decoration: InputDecoration(
+        labelText: _labelText,
+      ),
+    );
+  }
+
+  Row makeShowPassword() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        const Text('Show Password'),
+        Checkbox(
+          value: _showPassword,
+          onChanged: (bool? value) {
+            setState(() {
+              _showPassword = value ?? false;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  ElevatedButton makeLoginButton() {
+    return ElevatedButton(
+      onPressed: () {
+        // Implement your login logic here
+        final username = _usernameController.text;
+        final password = _passwordController.text;
+        // For now, just print the username and password
+        print('Username: $username, Password: $password');
+      },
+      child: const Text('Login'),
+    );
+  }
+
+  AppBar _AppBar() {
+    return AppBar(
+      title: const Text('Login Page'),
+      centerTitle: true,
+      leading: const Icon(Icons.menu),
+      actions: [
+        IconButton(onPressed: () {
+          print("Logout button pressed");
+        }, 
+        icon: const Icon(Icons.logout))
+      ],
+      backgroundColor: Colors.blue[100],
+    );
+  }
+}
+
